703e4fa37149227e56e9ef636041b8008a3bc5fb
= <rafael.borges2698@gmail.com>
2022-09-29 00:03:37 -0300

    added miragejs to fake api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import styled from 'styled-components'
+import { createServer } from 'miragejs'
 import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard'
 
 export function App() {
+
+  createServer({
+    routes() {
+      this.namespace = 'api'
+
+      this.get('/transactions', () => {
+        return [
+          {
+            id: 1,
+            title: 'Transaction 1',
+            amount: 400,
+            type: 'deposit',
+            category: 'food',
+            createAt: new Date()
+
+          }
+        ]
+      })
+    }
+  })
+
   return (
     <>
       <Header />
